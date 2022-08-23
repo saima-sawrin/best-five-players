52d1ec7424471d8861381ad85a0a15baf2b03e9d
Saima <saimasawrin324@gmail.com>
2022-08-23 12:26:19 +0600

    Validation

--- js/budget.js
+++ js/budget.js
@@ -5,6 +5,7 @@ document.getElementById('btn-calculate').addEventListener('click', function(){
     const playerExpense = perPlayer * totalPlayers;
     if(isNaN(perPlayer) || perPlayer == '' || perPlayer < 0 ){
         alert('Please enter a valid number');
+        return;
 
     }
      setTextElementById('expense-field', playerExpense);
@@ -12,13 +13,15 @@ document.getElementById('btn-calculate').addEventListener('click', function(){
 
 document.getElementById('btn-total').addEventListener('click', function(){
     const managerExpense =  getInputFieldElementById('manager-field');
-    if(isNaN(managerExpense) || managerExpense == ''  || perPlayer < 0 ){
+    if(isNaN(managerExpense) || managerExpense == '' ){
         alert('Please enter  a valid number');
+        return;
 
     }
     const coachExpense =  getInputFieldElementById('coach-field');
-    if(isNaN(coachExpense) || coachExpense == ''  || perPlayer < 0 ){
+    if(isNaN(coachExpense) || coachExpense == '' ){
         alert('Please enter a  valid number');
+        return;
 
     }
     const playerExpense = getTextElementById('expense-field')
